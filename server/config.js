6a2be7a7ecc8c339efb3099c6d815ee973b5fccb
rayyan <ra28yra46@gmail.com>
2026-05-16 13:44:13 +0400

    update2

--- server/config.js
+++ server/config.js
@@ -1,7 +1,6 @@
 ////Environment Variables
-import dotenv from "dotenv";
-
-dotenv.config(); 
+//import dotenv from "dotenv";
+//dotenv.config(); 
 export const PORT = process.env.PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
